7ce9a135646288f05aa1ba7f031369269ba86d64
Ale <alessandrochb@gmail.com>
2021-01-19 17:29:49 +0100

    stampate in pagina le cards con un foreach dei dati

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -2,13 +2,28 @@
 
 @section('content')
     {{-- <p>{{ $dresses }}</p> --}}
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
+    <div class="cards-container">
+        <div class="container">
+            <div class="row">
                 @foreach ($dresses as $key => $dress)
-                    <p>{{ $dress->marca }}</p>
-                @endforeach
-            </div>
+                    <div class="col-12 col-lg-4 text-center">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $dress->marca }}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">{{ $dress->tessuto }}</h6>
+                            <p>Colore: {{ $dress->colore }} </p>
+                            <p>Taglia: {{ $dress->taglia }} </p>
+                            <p>Modello: {{ $dress->modello }} </p>
+                            <p>Lunghezza: {{ $dress->lunghezza }} </p>
+                            <p class="text-center">
+                                Prezzo: {{ $dress->prezzo }} €
+                            </p>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi officiis minima earum, voluptate nam!.</p>
+                            <a href="#" class="card-link text-center">Buy now</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection

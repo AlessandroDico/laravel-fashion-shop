f7e30fb61cf6b69b27694f5f65fb0332eeed0dad
Ale <alessandrochb@gmail.com>
2021-01-19 15:51:43 +0100

    creato model ed estrapolato dati con funzione statica ::all()

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,10 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Dress;
+
 class HomeController extends Controller {
 
     public function getHome() {
+        $allDresses = Dress::all();
+
+        dd($allDresses); 
         return view('homePage');
+    }
+
+    public function getContacts() {
+        return view('contacts');
     }
 
 }
--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -8,7 +8,7 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
-        <h1>HOME PAGE</h1>
+        <h1>CONTACTS</h1>
 
 
     </body>
